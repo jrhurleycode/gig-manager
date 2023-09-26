178980bc09038f28d7a06aeb941f84bb0a711401
Josh Hurley <jrhurley5333@gmail.com>
2023-09-26 13:53:51 -0400

    init commit

---
+++ gig-manager-java/gigmanager/src/sql/dummy-data.sql
@@ -0,0 +1,10 @@
+CREATE TABLE gig (
+gig_id serial,
+gig_name varchar(50) NOT NULL,
+venue_name varchar (50) NOT NULL,
+gig_date date,
+doorsOpen_time time,
+soundCheck_time time,
+band_name varchar(50) NOT NULL,
+CONSTRAINT pk_gig PRIMARY KEY (gig_id)
+);
---
+++ gig-manager-java/gigmanager/src/test/java/com/gigmanager/gigmanager/GigmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gigmanager.gigmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GigmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
