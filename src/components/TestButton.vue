1f8d2a4799c844845a3034dc6608a00ea45ab3cc
Jason Hurley <paperjace@gmail.com>
2024-01-04 19:47:09 -0500

    Added testButton components and updated view and router to pull in the component

---
+++ src/components/TestButton.vue
@@ -0,0 +1,42 @@
+<script>
+// const API_ROOT = 'localhost:9000'
+export default {
+  data: () => ({
+    tableData: null
+  }),
+  methods: {
+    async getGigs () {
+      // data = await fetch(`${API_ROOT}/gigs`)
+      this.tableData = 'data has been got'
+      console.log('asdfasdf')
+    }
+  }
+}
+
+</script>
+
+<template>
+<div class="container">
+  <button type="button" @click="getGigs">Get Gigs</button>
+  <div class="dataOutput">{{ tableData }}</div>
+</div>
+</template>
+
+<style scoped>
+
+.container {
+  text-align: center;
+  width: 100%;
+}
+
+.dataOutput{
+  font-size: 2em
+}
+
+button {
+  font-size: 2em;
+  padding: .5em;
+  line-height: .5em;
+}
+
+</style>
---
+++ src/views/TestButton.vue
@@ -0,0 +1,10 @@
+<script setup>
+import TestButton from '../components/TestButton.vue'
+</script>
+
+<template>
+  <main>
+    <TestButton/>
+    
+  </main>
+</template>
