65a0024bddf64ed1e24c756b929ec6ef1813a9f3
Josh Hurley <jrhurley5333@gmail.com>
2024-02-18 13:13:32 -0500

    set up pinia files

---
+++ src/stores/gigStore.js
@@ -0,0 +1,9 @@
+import { defineStore } from 'pinia'
+
+export const useGigStore = defineStore('gigs', {
+
+  // You can think of state as the data of the store, getters as the computed properties of the store, and actions as the methods.  
+    state: () => {}
+}), 
+getters: {},
+actions: {}
